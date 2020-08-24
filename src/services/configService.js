e6208df498ef7126ce075cdadd07acd4fe1bd967
SwipeTuto <swipetuto@gmail.com>
2020-08-24 20:40:12 +0000

    t

--- src/services/configService.js
+++ src/services/configService.js
@@ -8,6 +8,6 @@ export const coudinaryBase = "https://res.cloudinary.com/hiimurmrd/image/upload/
 // export const base = "http://localhost:8000"
 
 export const BASEMEDIA = "http://localhost:8000/media/"
-// export const BASEMEDIA = "https://swipetuto-back.herokuapp.com/media/"
-export const baseURL = "https://swipetuto-back.herokuapp.com/api/v1/"
-export const base = "https://swipetuto-back.herokuapp.com"
+// export const BASEMEDIA = "https://swipetuto-back-dev.herokuapp.com/media/"
+export const baseURL = "https://swipetuto-back-dev.herokuapp.com/api/v1/"
+export const base = "https://swipetuto-back-dev.herokuapp.com"

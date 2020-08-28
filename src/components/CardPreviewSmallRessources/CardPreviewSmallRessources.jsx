501ff5e2761e9cfbd48bb2da9886854e9b273c07
Wikode Medias <wikodemedias@gmail.com>
2020-08-28 16:26:52 +0200

    ressource page redesign and tests

--- src/components/CardPreviewSmallRessources/CardPreviewSmallRessources.jsx
+++ src/components/CardPreviewSmallRessources/CardPreviewSmallRessources.jsx
@@ -3,12 +3,20 @@ import React from "react";
 import "./CardPreviewSmallRessources.scss";
 
 const CardPreviewSmallRessources = ({
-  ressource: { nom, description, lien },
+  ressource: { nom, description, lien, image },
 }) => {
+  console.log(image);
   return (
     <div className="CardPreviewSmallRessources">
       <a href={lien} target="_blank" rel="noopener noreferrer">
         <h4 className="title title-4">{nom}</h4>
+        <div className="CardPreviewSmallRessources__image">
+          <img
+            className="CardPreviewSmallRessources__image--image"
+            src={image}
+            alt="illustration"
+          />
+        </div>
         <p>{description}</p>
       </a>
     </div>

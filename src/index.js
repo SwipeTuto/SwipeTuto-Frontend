3af069c3260c0761dda519b8e117fc276bac57fb
SwipeTuto <swipetuto@gmail.com>
2020-10-25 09:08:46 +0000

    add google ana

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import store from './redux/store';
 import { authHeader } from "./helper/auth-header"
-import { getCookie } from "./helper/getCookie"
 
 import { BrowserRouter } from 'react-router-dom'
 

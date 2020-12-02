eeccddc17fefe3a28792e11e326f975939f39a5d
SwipeTuto <swipetuto@gmail.com>
2020-12-02 07:15:17 +0000

    last version

--- src/services/backOfficeService.js
+++ src/services/backOfficeService.js
@@ -11,17 +11,9 @@ export const sendEmailContact = data => {
 
   var config = { headers: { 'Content-Type': 'multipart/form-data' } }
 
-  // const data2 = {
-  //   email:  data.email,
-  //   categories: database.category,
-  //   message: data.description,
-  // }
-
-  // return client().post(`backoffice/contact/`,  JSON.stringify(data2)).then(rep => {
   return client().post(`backoffice/contact/`, bodyFormData, config).then(rep => {
     return rep
-    // history.push('/contact', history.location)
-    // history.go()
+
   }).catch(err => { 
     console.log(err.message)
     return err 

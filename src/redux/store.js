82c0ac537c051ee7d53d47e4573dfa6207a24135
Wikode Medias <wikodemedias@gmail.com>
2020-12-24 18:20:59 +0100

    bug mobile composeEnhancer

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,13 +3,13 @@ import rootReducer from './root-reducer';
 import thunk from 'redux-thunk';
 
 // DEV :
-let composeEnhancers;
-if (process.env.NODE_ENV === 'development') {
-  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-} else {
-  composeEnhancers = compose;
-}
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// let composeEnhancers;
+// if (process.env.NODE_ENV && process.env.NODE_ENV === 'development') {
+//   composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+// } else {
+//   composeEnhancers = compose;
+// }
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // ONLINE :
 // const composeEnhancers = compose;
